acfcf84c080dfb99e240c5f7d49b8f9384057f64
Jorlyna Pujols <jorlynapujols@Jorlynas-MBP.attlocal.net>
2023-11-07 11:39:40 -0500

    completed seed file for videos

---
+++ seeds/videoData.js
@@ -0,0 +1,31 @@
+const { Video } = require("../models")
+const videoData = [
+    {
+        video_url: "videourl.com",
+        description: "calming music",
+        author: "john doe",
+    },
+    {
+        video_url: "videourl.com",
+        description: "calming music",
+        author: "john doe",
+    },
+    {
+        video_url: "videourl.com",
+        description: "calming music",
+        author: "john doe",
+    },
+    {
+        video_url: "videourl.com",
+        description: "calming music",
+        author: "john doe",
+    },
+    {
+        video_url: "videourl.com",
+        description: "calming music",
+        author: "john doe",
+    },
+
+]
+const seedComments = () => Video.bulkCreate(videoData)
+module.exports = seedComments
