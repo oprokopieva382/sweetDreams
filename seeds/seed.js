6e281be65f0eab2691849e4e75f0998fe677553a
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-11-09 22:34:38 -0600

    add seeds of songs

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,10 @@
 const sequelize = require("../config/connection");
 const seedVideos = require("./videoData.js");
+const seedSongs = require("./songsData.js");
 const seedAll = async () => {
   await sequelize.sync({ force: true });
   await seedVideos();
+  await seedSongs();
   process.exit(0);
 };
 seedAll();
