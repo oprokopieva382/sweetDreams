363b90ebb5362da169130ee5ea1aefe2899268e1
Yasin Hubbard <yasin.hubbard@gmail.com>
2023-11-12 23:52:30 -0500

    likevideo in like.js changed to support try catch

--- public/js/likes.js
+++ public/js/likes.js
@@ -49,11 +49,33 @@ deleteSongIcons.forEach((icon) => {
 });
 
 //logic to handle like video with event listener and fetch request
-for (i of favIcon) {
-  i.addEventListener("click", function (e) {
-    like(e.target.dataset.id);
-  });
-}
+const likeVideoHandler = async (videoId) => {
+  try {
+    const response = await fetch(`/api/likes/videolike`, {
+      method: "POST",
+      body: JSON.stringify({ video_id: parseInt(videoId) }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    response.ok
+      ? console.log("Video liked")
+      : console.error("Failed to like the video");
+  } catch (error) {
+    console.error("Error occurred while liking the video", error);
+  }
+};
+
+likeSongIcons.forEach((icon) => {
+  icon.addEventListener("click", () => likeVideoHandler(icon.dataset.videoId));
+});
+
+// for (i of favIcon) {
+//   i.addEventListener("click", function (e) {
+//     like(e.target.dataset.id);
+//   });
+// }
 
 const like = async (id) => {
   const response = await fetch("/api/likes/videolike", {
