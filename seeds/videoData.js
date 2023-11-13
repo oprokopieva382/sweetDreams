060c180313f411aa47127e18046967fbd8a7acd7
Jorlyna Pujols <jorlynapujols@Jorlynas-MBP.attlocal.net>
2023-11-13 12:55:41 -0500

    added additional meditation videos

--- seeds/videoData.js
+++ seeds/videoData.js
@@ -13,12 +13,23 @@ const videoData = [
     author: "Goodful",
     run_time: 10
   },
-
+  {
+    video_url: "https://www.youtube.com/embed/USPJLg_mcps?si=zX9MraALXrlot5z7",
+    title: "Align With What You Desire",
+    author: "GM",
+    run_time: 10
+  },
   {
     video_url: "https://www.youtube.com/embed/YdDv_0kwsb8",
     title: "Sleep",
     author: "GM",
     run_time: 10
+  },
+  {
+    video_url: "https://www.youtube.com/embed/aqyj_U79ejA?si=6LjinRLPPeukYSI-",
+    title: "Cultivating Self-Love & Self-Acceptance",
+    author: "GM",
+    run_time: 16
   },
   {
     video_url: "https://www.youtube.com/embed/Tr3Uw1UmTUk",
@@ -31,6 +42,25 @@ const videoData = [
     title: "Positive Sleep Guided Meditation",
     author: "GM",
     run_time: 20
+  },
+  {
+    video_url: "https://www.youtube.com/embed/GTdEJwVOxg0?si=_JWX5Gkhy1mCxrBf",
+    title: "Inner Peace and Deep Relaxation",
+    author: "GM",
+    run_time: 20
+  },
+  {
+    video_url: "https://www.youtube.com/embed/OfHsOmFZZyI?si=TJZTQ7mzGQz5aQNT",
+    title: "Positive Affirmations. While You Sleep",
+    author: "GM",
+    run_time: 21
+  },
+  
+  {
+    video_url: "https://www.youtube.com/embed/2DXqMBXmP8Q?si=VKVMqlAoqVpRbYsO",
+    title: "The Powerful Release of Letting Go!",
+    author: "GM",
+    run_time: 28
   },
  
 ];
