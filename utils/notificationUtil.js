dccd48872aefa8efdb9d56c859cc1ad7fd898bcb
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-11-13 18:13:37 -0600

    wireframe of js functions for pop up windows

---
+++ utils/notificationUtil.js
@@ -0,0 +1,7 @@
+const alertSuccessLogin = document.querySelectorAll(".alert-success-login");
+const alertSuccessLike = document.querySelectorAll(".alert-success-like");
+const alertDanger = document.querySelectorAll(".alert-danger");
+
+const successLogin = () => {};
+const successLike = () => {};
+const alertDangerMessage = () => {};
